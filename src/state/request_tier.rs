47b0b5a8f3f3f7e246d2683b80b7f7050e487963
Hema <96527414+kwdila@users.noreply.github.com>
2025-11-19 17:30:14 +0300

    refactor: use per bundle tier constraints not global

--- src/state/request_tier.rs
+++ src/state/request_tier.rs
@@ -16,6 +16,47 @@ unsafe impl Pod for RequestTier {}
 impl RequestTier {
     const ALL: [RequestTier; 3] = [RequestTier::Eco, RequestTier::Standard, RequestTier::Pro];
 
+    pub fn get_verifiers_per_auction(&self) -> u64 {
+        match self {
+            RequestTier::Eco => 3,
+            RequestTier::Standard => 3,
+            RequestTier::Pro => 3,
+        }
+    }
+
+    pub fn get_bid_reveal_duration(&self) -> u64 {
+        match self {
+            RequestTier::Eco => 3,
+            RequestTier::Standard => 3,
+            RequestTier::Pro => 3,
+        }
+    }
+
+    pub fn get_active_auction_duration(&self) -> u64 {
+        match self {
+            RequestTier::Eco => 3,
+            RequestTier::Standard => 3,
+            RequestTier::Pro => 3,
+        }
+    }
+
+    pub fn get_bundle_duration(&self) -> u64 {
+        match self {
+            RequestTier::Eco => 150,
+            RequestTier::Standard => 150,
+            RequestTier::Pro => 150,
+        }
+    }
+
+    /// The maximum number of requests per bundle
+    pub fn get_request_per_bundle(&self) -> u64 {
+        match self {
+            RequestTier::Eco => 1,
+            RequestTier::Standard => 1,
+            RequestTier::Pro => 1,
+        }
+    }
+
     /// Maximum allowed context length (in tokens) per tier
     pub fn get_max_context_length_tokens(&self) -> u64 {
         match self {
