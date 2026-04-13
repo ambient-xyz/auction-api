27ab27c9a71135d91da8574087bd12f302002879
Hema <96527414+kwdila@users.noreply.github.com>
2026-04-13 14:54:56 +0300

    Defer policy layout selection

--- src/state/config_policy_v2.rs
+++ src/state/config_policy_v2.rs
@@ -1,4 +1,4 @@
-use super::{AccountLayoutVersion, Pubkey};
+use super::Pubkey;
 use bytemuck::{Pod, Zeroable};
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
@@ -22,9 +22,7 @@ pub struct ConfigPolicyV2 {
     pub bump: u64,
     pub minimum_bundle_auction_pairs: u64,
     pub policy_flags: u64,
-    pub default_bundle_escrow_version: u8,
-    pub default_bundle_verifier_page_version: u8,
-    pub _reserved0: [u8; 6],
+    pub _reserved0: [u8; 8],
     pub admin_authorities: [Pubkey; CONFIG_POLICY_V2_ADMIN_CAPACITY],
     pub service_authorities: [Pubkey; CONFIG_POLICY_V2_SERVICE_CAPACITY],
     pub reserved: [[u8; 32]; 24],
@@ -36,9 +34,7 @@ impl Default for ConfigPolicyV2 {
             bump: 0,
             minimum_bundle_auction_pairs: 2,
             policy_flags: 0,
-            default_bundle_escrow_version: AccountLayoutVersion::V1 as u8,
-            default_bundle_verifier_page_version: AccountLayoutVersion::V1 as u8,
-            _reserved0: [0; 6],
+            _reserved0: [0; 8],
             admin_authorities: [Pubkey::default(); CONFIG_POLICY_V2_ADMIN_CAPACITY],
             service_authorities: [Pubkey::default(); CONFIG_POLICY_V2_SERVICE_CAPACITY],
             reserved: [[0; 32]; 24],
@@ -48,12 +44,4 @@ impl Default for ConfigPolicyV2 {
 
 impl ConfigPolicyV2 {
     pub const LEN: usize = std::mem::size_of::<ConfigPolicyV2>();
-
-    pub fn default_bundle_escrow_layout_version(&self) -> Option<AccountLayoutVersion> {
-        AccountLayoutVersion::try_from(self.default_bundle_escrow_version).ok()
-    }
-
-    pub fn default_bundle_verifier_page_layout_version(&self) -> Option<AccountLayoutVersion> {
-        AccountLayoutVersion::try_from(self.default_bundle_verifier_page_version).ok()
-    }
 }
